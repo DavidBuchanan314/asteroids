32ed96d0e67f224fb3ecae21d9c1e5217e2364e1
DavidBuchanan314 <d@vidbuchanan.co.uk>
2017-01-27 20:06:11 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # asteroids
 Yet another "Asteroids" clone, with Three.js
+
+Very much a work-in-progress...
---
+++ main.js
@@ -0,0 +1,106 @@
+"use strict";
+
+/* Keyboard Stuff */
+
+var key = {};
+window.onkeyup = function(e) { key[e.keyCode] = false; };
+window.onkeydown = function(e) { key[e.keyCode] = true; };
+
+var code = {
+	UP: 87,
+	DOWN: 83,
+	LEFT: 65,
+	RIGHT: 68,
+	FIRE: 32
+}
+
+/* Game State Vars*/
+
+var player = {
+	rotVel: 0,
+	dampRot: 0.85,
+	dampVel: 0.90,
+	vel: {x:0, y:0},
+	accel: 0.5,
+	rotAccel: 0.03
+};
+var asteroids = [];
+
+/* Three.js Stuff */
+
+var size = Math.min(window.innerWidth, window.innerHeight); // square aspect ratio;
+var gameSize = 512; // relative value
+
+var camera, scene, renderer;
+
+function init() {
+	camera = new THREE.PerspectiveCamera( 90, 1, 1, 1000 );
+	camera.position.z = gameSize/2;
+
+	/* Player Setup */
+
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push( new THREE.Vector3( 0,  12,  0) ); // front
+	geometry.vertices.push( new THREE.Vector3( 0,  -2,  0) ); // back
+	geometry.vertices.push( new THREE.Vector3(-7, -5,  0) ); // left
+	geometry.vertices.push( new THREE.Vector3( 7, -5,  0) ); // right
+	geometry.faces.push( new THREE.Face3( 0, 2, 1 ) );
+	geometry.faces.push( new THREE.Face3( 0, 1, 3 ) );
+	
+	var material = new THREE.MeshBasicMaterial( {color: 0x000000, side: THREE.DoubleSide} );
+	
+	player.mesh = new THREE.Mesh( geometry, material );
+	
+	/* End Player Setup */
+	
+	scene = new THREE.Scene();
+	scene.add( player.mesh );
+	
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( size, size );
+	renderer.setClearColor( 0xffffff, 1 );
+	document.body.appendChild( renderer.domElement );
+
+	loop();
+}
+
+function update() {
+	if (key[code.UP]) {
+		player.vel.x -= player.accel * Math.sin(player.mesh.rotation.z);
+		player.vel.y += player.accel * Math.cos(player.mesh.rotation.z);
+	}
+	if (key[code.DOWN]) {
+		player.vel.x += player.accel * Math.sin(player.mesh.rotation.z);
+		player.vel.y -= player.accel * Math.cos(player.mesh.rotation.z);
+	}
+	if (key[code.LEFT])  player.rotVel += player.rotAccel;
+	if (key[code.RIGHT]) player.rotVel -= player.rotAccel;
+
+	player.rotVel *= player.dampRot;
+	player.vel.x *= player.dampVel;
+	player.vel.y *= player.dampVel;
+
+	player.mesh.rotation.z += player.rotVel;
+	player.mesh.position.x += player.vel.x;
+	player.mesh.position.y += player.vel.y;
+	
+	/* Player Wraparound */
+	if (player.mesh.position.x >  gameSize/2) player.mesh.position.x -= gameSize;
+	if (player.mesh.position.x < -gameSize/2) player.mesh.position.x += gameSize;
+	if (player.mesh.position.y >  gameSize/2) player.mesh.position.y -= gameSize;
+	if (player.mesh.position.y < -gameSize/2) player.mesh.position.y += gameSize;
+}
+
+function render() {
+	renderer.render( scene, camera );
+}
+
+function loop() {
+	requestAnimationFrame( loop );
+	
+	update();
+	render();
+}
+
+window.onload = init;
